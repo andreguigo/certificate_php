79247a2c7b0cad7bbe5bc21390fa3497d26133d1
andreguigo <andreguigo@gmail.com>
2021-05-12 15:12:41 -0300

    Melhorias textuais e remoção de comentários

--- model/lib/Autoload.php
+++ model/lib/Autoload.php
@@ -1,9 +1,4 @@
 <?php
-// This is an autoloader for the crud-pdo! SDK.
-// If you are not using a global autoloader do the following
-// before any other crud-pdo files:
-// 
-// require_once "<path>/Autoload.php"
 
 function certificateAutoload($classname) {
 	require $classname.".php";
--- test/add-certificate.php
+++ test/add-certificate.php
@@ -2,7 +2,7 @@
 
 define("lib", "../model/");
 
-require_once lib."lib/Autoload.php"; // Remove if using a global autoloader
+require_once lib."Lib/Autoload.php";
 require_once lib."Certificate.php";
 
 $tml = new Certificate;
@@ -11,7 +11,6 @@ $tml->name           = "Teste de Teste";
 $tml->email          = "teste@teste.com";
 $tml->testimonial_id = "2";
 
-// recommended not to modify
 date_default_timezone_set('America/Sao_Paulo');
 $tml->licensed  = substr(md5(date('l jS \of F Y h:i:s A')), 0, 5);
 $tml->date_sign = date('Y-m-d');
